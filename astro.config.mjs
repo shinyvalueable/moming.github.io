75fe582fc1b6e597460c747a38d7eecb34065dda
cn <2790463329@qq.com>
2026-08-17 11:56:37 +0800

    Update GitHub Pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://github.com/shinyvalueable.github.io",
-  base: "/moming/",
+  site: "https://nachengmerge.github.io",
 });
